952e37e15f0402e29e11715d3b3dd65712bf4e35
akash gund <gund.a@husky.neu.edu>
2017-11-23 23:12:53 -0500

    workqueue modified

--- AEDFinalProject/src/Business/WorkQueue/VaccineWorkRequest.java
+++ AEDFinalProject/src/Business/WorkQueue/VaccineWorkRequest.java
@@ -9,6 +9,45 @@ package Business.WorkQueue;
  *
  * @author akash
  */
-public class VaccineWorkRequest {
+public class VaccineWorkRequest extends WorkRequestABS {
+    private String VaccineName;
+    private int quantityReq;
+    private double bill;
+    private String paymentStatus;
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getVaccineName() {
+        return VaccineName;
+    }
+
+    public void setVaccineName(String VaccineName) {
+        this.VaccineName = VaccineName;
+    }
+
+    public int getQuantityReq() {
+        return quantityReq;
+    }
+
+    public void setQuantityReq(int quantityReq) {
+        this.quantityReq = quantityReq;
+    }
+
+    public double getBill() {
+        return bill;
+    }
+
+    public void setBill(double bill) {
+        this.bill = bill;
+    }
+    
+    
+    
     
 }
